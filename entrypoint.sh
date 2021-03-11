47823b8e146919e655d18ca84ce031c0bbf6b119
M1DN1GHT01 <noah@noahserver.online>
2021-03-11 09:27:04 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 cd /home/container
 
@@ -5,8 +6,9 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=$(echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+START_COMMAND=$(echo -e ${MODIFIED_STARTUP})
+echo -e ":/home/container$ ${START_COMMAND}"
 
 # Run the Server
-eval ${MODIFIED_STARTUP}
+${MODIFIED_STARTUP}
