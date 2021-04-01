f58541a6be39d810e8033c16b181183dedb13647
M1DN1GHT01 <noah@noahserver.online>
2021-04-01 10:24:16 -0400

    Main Pavlov Update Docker

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# ----------------------------------
+# Environment: debian:buster-slim 
+# Pavlov Update Server
+# ----------------------------------
+FROM quay.io/m1dn1ght/main:Debian-Enhanced
+
+LABEL author="Noah Smith" maintainer="noah@noahserver.online"
+
+## install reqs
+RUN dpkg --add-architecture i386 \
+ && apt update \
+ && apt upgrade -y \
+ && apt install -y libssl1.1:i386 libtinfo6:i386 libtbb2:i386 libtinfo5:i386 libcurl4-gnutls-dev:i386 libcurl4:i386 libncurses5:i386 libcurl3-gnutls:i386 libtcmalloc-minimal4:i386 faketime:i386 libtbb2:i386 \
+    lib32tinfo6 lib32stdc++6 lib32z1 libtbb2 libtinfo5 libstdc++6 libreadline5 libncursesw5 libfontconfig1 libnss-wrapper gettext-base libc++-dev
+
+## install rcon
+RUN cd /tmp/ \
+ && curl -sSL https://github.com/gorcon/rcon-cli/releases/download/v0.9.0/rcon-0.9.0-amd64_linux.tar.gz > rcon.tar.gz \
+ && tar xvf rcon.tar.gz \
+ && mv rcon-0.9.0-amd64_linux/rcon /usr/local/bin/
+
+USER        container
+ENV         HOME /home/container
+WORKDIR     /home/container
+
+COPY ./entrypoint.sh /entrypoint.sh
---
+++ entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+cd /home/container
+sleep 1
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+## just in case someone removed the defaults.
+if [ "${STEAM_USER}" == "" ]; then
+    echo -e "steam user is not set.\n"
+    echo -e "Using anonymous user.\n"
+    STEAM_USER=anonymous
+    STEAM_PASS=""
+    STEAM_AUTH=""
+else
+    echo -e "user set to ${STEAM_USER}"
+fi
+
+## if auto_update is not set or to 1 update
+if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then 
+    # Update Source Server
+    if [ ! -z ${SRCDS_APPID} ]; then
+        ./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH}  +force_install_dir /home/container +app_update ${SRCDS_APPID} $( [[ -z ${SRCDS_BETAID} ]] || printf %s "-beta ${SRCDS_BETAID}" ) $( [[ -z ${SRCDS_BETAPASS} ]] || printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ -z ${HLDS_GAME} ]] || printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ -z ${VALIDATE} ]] || printf %s "validate" ) +quit
+    else
+        echo -e "No appid set. Starting Server"
+    fi
+
+else
+    echo -e "Not updating game server as auto update was set to 0. Starting Server"
+fi
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
